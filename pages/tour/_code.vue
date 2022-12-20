409561ca78421adf8831e14664f661e30ed97db1
thanhhung0096 <thanhhung.0096@gmail.com>
2022-12-20 10:24:39 +0700

    init

---
+++ pages/tour/_code.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="height: 100%; width: 100%">
+    <h1>Hello {{ tourCode }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    return {
+      tourCode: params.code,
+    };
+  },
+
+  data() {
+    return {
+      tourCode: "",
+    };
+  },
+};
+</script>
